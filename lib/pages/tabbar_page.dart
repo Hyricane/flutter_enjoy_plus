5cccea4482b916f5224f73a8b85ec55a3bf74f7d
猛男 <xxx@hm.com>
2025-07-18 17:13:56 +0800

    搭建tabbar

--- lib/pages/tabbar_page.dart
+++ lib/pages/tabbar_page.dart
@@ -8,12 +8,57 @@ class TabbarPage extends StatefulWidget {
 }
 
 class _TabbarPageState extends State<TabbarPage> {
+  List tabsList = [
+    {
+      'label': '首页',
+      'icon': '/tabs/home_default.png',
+      'activeIcon': '/tabs/home_active.png'
+    },
+    {
+      'label': '我的',
+      'icon': '/tabs/my_default.png',
+      'activeIcon': '/tabs/my_active.png'
+    }
+  ]; // 底部导航栏数据
+
+  int activeIndex = 0; // 默认选中的索引
+
+  List<BottomNavigationBarItem> getTabsBar() {
+    List<BottomNavigationBarItem> list = [];
+    for (var i = 0; i < tabsList.length; i++) {
+      list.add(
+        BottomNavigationBarItem(
+            label: tabsList[i]["label"],
+            icon: Image.asset(
+              tabsList[i]["icon"],
+              width: 30,
+              height: 30,
+            ),
+            activeIcon: Image.asset(
+              tabsList[i]["activeIcon"],
+              width: 30,
+              height: 30,
+            )),
+      );
+    }
+    return list;
+  } // 创建底部导航栏
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'TabbarPage',
-        style: TextStyle(decoration: TextDecoration.none),
+    return Scaffold(
+      body: Text("TabbarPage"),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: activeIndex,
+        selectedItemColor: const Color.fromARGB(255, 85, 145, 175),
+        unselectedItemColor: Colors.black,
+        items: getTabsBar(),
+        onTap: (int index) {
+          setState(() {
+            activeIndex = index;
+          });
+        },
       ),
     );
   }
