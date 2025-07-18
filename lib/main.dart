589da76d1355dae95208e20f52e32c06f54f60c2
猛男 <xxx@hm.com>
2025-07-18 16:25:03 +0800

    创建tabbarPage-配置路由

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:enjoy_plus_flutter_7/pages/tabbar_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Center(
-      child: Text('Hello World'),
-    ),
+    // 命名路由 跳转页面
+    routes: {
+      '/': (context) => TabbarPage(),
+    },
+    // initialRoute: '/',
+    // home: Center(
+    //   child: Text('Hello World'),
+    // ),
   ));
 }
---
+++ lib/pages/tabbar_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TabbarPage extends StatefulWidget {
+  const TabbarPage({Key? key}) : super(key: key);
+
+  @override
+  _TabbarPageState createState() => _TabbarPageState();
+}
+
+class _TabbarPageState extends State<TabbarPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('TabbarPage'),
+    );
+  }
+}
